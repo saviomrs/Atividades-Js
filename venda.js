bb37875c99b9ef588cee80569ecc8f1cfe277fd0
Sávio Oliveira <92489899+saviomrs@users.noreply.github.com>
2021-11-04 06:58:09 -0300

    Update venda.js

--- venda.js
+++ venda.js
@@ -1,20 +1,24 @@
 var frmModelo;
-var frmValor= parseInt();
+var frmValor;
 var divMostrar;
 
 //Botao cadastrar add evento
 btnSimular.addEventListener('click', function(){
-    frmModelo = modelo.value 
-    frmValor = valor.value
-    divMostrar = document.getElementById('mostrarSimulacao')
-    alert('Simulação em andamento!')
+    frmModelo = dadoVeiculo.value // modelo do carro
+    frmValor = dadoPreco.value // preco do carro
+    frmParcelas = parseInt(qtdParcelas.value) // qtd de parcela
+
+    divMostrar = document.getElementById('mostrarSimulacao') // mostrar as informacoes na tela
+    alert('Simulação em processamento!') // processando
+
+    parcela = ((frmValor/2)/frmParcelas) // valor da parcela sem juros
+    parcelaComJuros = parcela*1.70 //cada parcela sem juros deve ser acrescida de 70% (juros)
    
     divMostrar.innerHTML = `
-        <div>Modelo do Veículo:  <strong>${frmModelo}</strong></div>
-        <div>Valor: R$ <strong>${frmValor}</strong></div>
-        <div>Valor da entrada: R$ <strong>${frmValor/2}</strong></div>
-        <div>60 Parcelas no valor de R$: <strong>${((((frmValor/2)/60)*0.70)+((frmValor/2)/60)).toFixed(2)}</strong></div>
-        
+        <div>Modelo: <strong>${frmModelo}</strong></div>
+        <div>Valor: <strong>R$ ${frmValor}</strong></div>
+        <div>Valor da entrada: <strong> R$ ${frmValor/2}</strong></div>
+        <div>${frmParcelas} parcelas no valor de R$ <strong>${parcelaComJuros.toFixed(2)}</strong></div>
   `
     
 })
