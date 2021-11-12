336f1bf9fe4de650ccbff4d52278a686069f2637
Sávio Oliveira <92489899+saviomrs@users.noreply.github.com>
2021-11-12 09:16:22 -0300

    Add files via upload

---
+++ coelho.js
@@ -0,0 +1,34 @@
+const inputCoelhos = document.getElementById('inputCoelhos');
+const inputAno = document.getElementById('inputAno');
+const btnCoelhos = document.getElementById('btnCoelhos');
+const outCoelhos = document.getElementById('outCoelhos');
+
+
+
+function mostarPrevisao() {
+    
+    let numeroCoelhos = Number(inputCoelhos.value);
+    let numeroDeAnos = Number(inputAno.value);
+
+    let total = numeroCoelhos;
+    let quantidade = '';
+
+    if (numeroCoelhos < 2 || numeroDeAnos == 0 || isNaN(numeroCoelhos) || isNaN(numeroDeAnos)) {
+        alert('Preencha os campos corretamente');
+        inputCoelhos.value = '';
+        inputAno.value = '';
+        inputCoelhos.focus();
+        return;
+    }
+
+    for (let index = 1; index <= numeroDeAnos; index++) {
+        quantidade += `${index}º Ano: ${total} Coelhos.\n`;
+        total *= 3;
+        
+    }
+
+    outCoelhos.textContent = quantidade;
+
+}
+
+btnCoelhos.addEventListener('click', mostarPrevisao);
